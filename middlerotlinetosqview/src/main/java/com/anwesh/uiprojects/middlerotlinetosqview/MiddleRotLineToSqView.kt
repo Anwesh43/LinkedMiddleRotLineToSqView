f4627d82cade2b49af3596a48296c3d5483eb4c6
Anwesh43 <anweshthecool0@gmail.com>
2020-05-29 06:08:35 +0530

    adding MiddleRotLineToSqView class

--- middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
+++ middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawMRLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class MiddleRotLineToSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
