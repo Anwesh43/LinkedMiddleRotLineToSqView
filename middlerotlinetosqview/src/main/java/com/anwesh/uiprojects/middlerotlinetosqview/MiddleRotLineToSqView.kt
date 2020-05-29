726daa3d5fc62b983c400c1ee71d8cca957c17d2
Anwesh43 <anweshthecool0@gmail.com>
2020-05-29 06:14:46 +0530

    adding MRLNode class

--- middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
+++ middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
@@ -121,4 +121,45 @@ class MiddleRotLineToSqView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MRLNode(var i : Int, val state : State = State()) {
+
+        private var next : MRLNode? = null
+        private var prev : MRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = MRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MRLNode {
+            var curr : MRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
