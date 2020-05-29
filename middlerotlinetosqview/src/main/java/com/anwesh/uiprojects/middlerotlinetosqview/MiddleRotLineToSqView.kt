eb64adc17c8b4d5073bcceb8e3efa193a9ae0fd1
Anwesh43 <anweshthecool0@gmail.com>
2020-05-29 06:22:16 +0530

    adding renderer in view

--- middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
+++ middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
@@ -61,14 +61,16 @@ fun Canvas.drawMRLNode(i : Int, scale : Float, paint : Paint) {
 
 class MiddleRotLineToSqView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -192,7 +194,7 @@ class MiddleRotLineToSqView(ctx : Context) : View(ctx) {
         private val mrl : MiddleRotLineToSquare = MiddleRotLineToSquare(0)
         private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             mrl.draw(canvas, paint)
             animator.animate {
