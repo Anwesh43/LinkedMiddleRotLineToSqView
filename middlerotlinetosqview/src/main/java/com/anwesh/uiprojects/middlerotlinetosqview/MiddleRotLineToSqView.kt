3970d9d8cd38bfc14e52e1a884aed421defb6466
Anwesh43 <anweshthecool0@gmail.com>
2020-05-29 06:07:12 +0530

    adding Canvas extension method

--- middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
+++ middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
@@ -21,8 +21,38 @@ val sizeFactor : Float = 2.9f
 val strokeFactor : Float = 90f
 val colors : Array<String> = arrayOf("#4CAF50", "#673AB7", "#009688", "#2196F3", "#F44336")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawMiddleRotLine(i : Int, sf : Float, size : Float, paint : Paint) {
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    val si : Float = 1f - 2 * i
+    save()
+    rotate(-rot * sf1 * si)
+    drawLine(0f, 0f, 0f, -size, paint)
+    restore()
+    save()
+    translate(-size + size * i, 0f)
+    drawRect(RectF(0f, -size * sf2, size, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawMRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        drawMiddleRotLine(i, scale.sinify(), size, paint)
+    }
+    restore()
+}
