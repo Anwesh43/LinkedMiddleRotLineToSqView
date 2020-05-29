f9e27d889b8f5088d4d12f74006e2fef6dded220
Anwesh43 <anweshthecool0@gmail.com>
2020-05-29 06:11:41 +0530

    adding Animator class

--- middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
+++ middlerotlinetosqview/src/main/java/com/anwesh/uiprojects/middlerotlinetosqview/MiddleRotLineToSqView.kt
@@ -22,6 +22,8 @@ val strokeFactor : Float = 90f
 val colors : Array<String> = arrayOf("#4CAF50", "#673AB7", "#009688", "#2196F3", "#F44336")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val delay : Long = 20
+
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -88,6 +90,34 @@ class MiddleRotLineToSqView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
